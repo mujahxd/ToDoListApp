09ae4666abde9ffd1ee66718b6cc4c5e313766e0
Mujahid <mujahid.170997@gmail.com>
2023-03-16 20:07:00 +0700

    tambah feature register

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"todolistapp/config"
@@ -13,18 +14,52 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var user = user.UserModel{}
-	user.SetSQLConnection(db)
+	var userModel = user.UserModel{}
+	var user = user.User{}
+	userModel.SetSQLConnection(db)
 
 	var menu int
+outerloop:
 	for menu != 99 {
 		fmt.Println("Selamat Datang di Aplikasi ToDo List")
 		fmt.Println("1. Login")
 		fmt.Println("2. Register")
-		fmt.Println("0. Exit")
+		fmt.Println("99. Exit")
 		fmt.Print("Pilih menu: ")
 		fmt.Scanln(&menu)
+		switch menu {
+		// fitur login
+		case 1:
+		// fitur register
+		case 2:
+			fmt.Print("Masukkan Username: ")
+			fmt.Scanln(&user.Username)
+			fmt.Print("Masukkan Password: ")
+			fmt.Scanln(&user.Password)
+			fmt.Print("Masukkan Fullname: ")
+			fmt.Scanln(&user.FullName)
+			err := register(db, user.Username, user.Password, user.Password)
+			if err != nil {
+				fmt.Printf("Username sudah ada, GAGAL membuat Akun\n\n")
 
+			} else {
+
+				fmt.Printf("User BERHASIL dibuat!\n\n")
+			}
+
+		case 99:
+			break outerloop
+		}
 	}
 
+}
+
+func register(db *sql.DB, username, password, fullname string) error {
+	sqlStatement := "INSERT INTO public.\"User\" (username, password, full_name) VALUES ($1, $2, $3)"
+	_, err := db.Exec(sqlStatement, username, password, fullname)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
